de581ff68f6b6f09128548e15a2b88d0a3c9a959
dsivanov_5372 <mitya5372@yandex.ru>
2023-02-06 17:28:57 +0300

    realy final improvements

--- gateway/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ gateway/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -4,13 +4,11 @@ import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
 
 @Data
 @Builder
 public class UserDto {
     Long id;
-    @NotBlank
     String name;
     @Email(message = "Invalid email address!")
     String email;
